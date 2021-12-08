57beb58c32d2e609f681ecb7bd64d9a0463e8b11
joseleii <Phambaodu25072001@gmail.com>
2021-12-08 17:59:43 +0700

    bắt đầu làm phân quyền

--- src/core/auth.py
+++ src/core/auth.py
@@ -1,25 +1,50 @@
-from flask import Flask, request, jsonify, make_response, request, render_template, session, flash
+from flask import Flask, request, jsonify, make_response, request
 import jwt
-from datetime import datetime, timedelta
+from flask_jwt_extended import jwt_required, current_user, get_jwt_identity, get_raw_jwt
 from functools import wraps
-su = 'Thisisasecret!'
+from ..models.accountDb import AccountDb
+# su = 'Thisisasecret!'
+#
+#
+# def token_required(func):
+#     # decorator factory which invoks update_wrapper() method and passes decorated function as an argument
+#     @wraps(func)
+#     def decorated(*args, **kwargs):
+#         token = request.args.get('token')
+#         if not token:
+#             return jsonify({'Alert!': 'Token is missing!'}), 401
+#
+#         try:
+#
+#             data = jwt.decode(token, su)
+#         # You can use the JWT errors in exception
+#         # except jwt.InvalidTokenError:
+#         #     return 'Invalid token. Please log in again.'
+#         except:
+#             return jsonify({'Message': 'Invalid token'}), 403
+#         return func(*args, **kwargs)
+#     return decorated
 
 
-def token_required(func):
-    # decorator factory which invoks update_wrapper() method and passes decorated function as an argument
+@jwt_required
+def get_id_from_jwt_token():
+    id = get_jwt_identity()
+    return id
+
+
+def authorized_required(func, roles):
     @wraps(func)
     def decorated(*args, **kwargs):
-        token = request.args.get('token')
-        if not token:
-            return jsonify({'Alert!': 'Token is missing!'}), 401
-
         try:
-
-            data = jwt.decode(token, su)
-        # You can use the JWT errors in exception
-        # except jwt.InvalidTokenError:
-        #     return 'Invalid token. Please log in again.'
+            user_id = get_id_from_jwt_token()
+            user = AccountDb.find_by_id(user_id)
+            ok = False
+            for x in roles:
+                if user.roleId == x:
+                    ok = True
+            if not ok:
+                raise Exception()
         except:
-            return jsonify({'Message': 'Invalid token'}), 403
+            return {'message': "You are not authorized to perform this activity"}, 403
         return func(*args, **kwargs)
     return decorated
