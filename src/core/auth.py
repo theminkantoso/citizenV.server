fde89f5450a87707b8905590a526735ed9847453
joseleii <Phambaodu25072001@gmail.com>
2021-12-11 19:35:00 +0700

    Update auth.py

--- src/core/auth.py
+++ src/core/auth.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, make_response, request
 import jwt
-from flask_jwt_extended import jwt_required, current_user, get_jwt_identity, get_raw_jwt
+from flask_jwt_extended import jwt_required, current_user, get_jwt_identity, get_jwt
 from functools import wraps
 from ..models.accountDb import AccountDb
 # su = 'Thisisasecret!'
