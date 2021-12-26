8fd34976964a2e35e37ca0051ae605e7f9488899
joseleii <Phambaodu25072001@gmail.com>
2021-12-26 11:15:56 +0700

    Update file.py

--- src/controller/file.py
+++ src/controller/file.py
@@ -1,8 +1,7 @@
-from io import BytesIO
 from flask import send_file
 from flask_restful import Resource
 from src.models.fileDb import FileDb
-from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import jwt_required
 from src.core.auth import authorized_required
 
 
