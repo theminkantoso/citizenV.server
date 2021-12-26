26697d51a445276ec62fa434477bfd209be63cbe
tuyenshin2004 <71513781+tuyenshin2004@users.noreply.github.com>
2021-12-26 10:51:53 +0700

    sửa lại như cũ

--- src/controller/file.py
+++ src/controller/file.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from flask import send_file, send_from_directory
+from flask import send_file
 from flask_restful import Resource
 from src.models.fileDb import FileDb
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
@@ -8,8 +8,8 @@ from src.core.auth import authorized_required
 
 class File(Resource):
 
-    # @jwt_required()
-    # @authorized_required(roles=[4])
+    @jwt_required()
+    @authorized_required(roles=[4])
     def get(self):
         file = FileDb.file()[0]
         return send_file(file, as_attachment=True, mimetype='application/pdf', download_name=file)
